93ce3a751758c8ae9c0cd0df3e3dc9ac3a7316bd
steveoh <sgourley@utah.gov>
2017-06-28 09:23:41 -0600

    lint and update requires

--- test/test_avy_data.js
+++ test/test_avy_data.js
@@ -1,15 +1,15 @@
-var parser = require('../parser');
-var forecast = require('../forecast');
-var utahavy = require('../utahavy');
-var expect = require('chai').expect;
-var fs = require('fs');
-var express = require('express');
-var request = require('supertest');
+const parser = require('../src/parser');
+const forecast = require('../src/forecast');
+const utahavy = require('../src/utahavy');
+const expect = require('chai').expect;
+const fs = require('fs');
+const express = require('express');
+const request = require('supertest');
 
 describe('utahavy', function () {
-    var server;
+    let server;
 
-    beforeEach(function() {
+    beforeEach(function () {
         var app = express();
         utahavy.express({
             expressApp: app,
@@ -17,45 +17,49 @@ describe('utahavy', function () {
             checkCert: false
         });
 
-        server = app.listen(3000);
+        const port = 3000;
+        server = app.listen(port);
     });
 
-    afterEach(function() {
+    afterEach(function () {
         server.close();
     });
 
-    it('responds to invalid data', function() {
-      return request(server)
-        .post('/utahavy')
-        .send({})
-        .expect(200).then(function(response) {
-          return expect(response.body).to.eql({
-            version: '1.0',
-            response: {
-              directives: [],
-              shouldEndSession: true,
-              outputSpeech: {
-                type: 'SSML',
-                ssml: '<speak>Sorry, something bad happened INVALID_REQUEST_TYPE</speak>'
-              }
-            },
-            sessionAttributes: {}
-          });
-        });
+    it('responds to invalid data', function () {
+        return request(server)
+            .post('/utahavy')
+            .send({})
+            .expect(200).then(function (response) {
+                return expect(response.body).to.eql({
+                    version: '1.0',
+                    response: {
+                        directives: [],
+                        shouldEndSession: true,
+                        outputSpeech: {
+                            type: 'SSML',
+                            ssml: '<speak>Sorry, something bad happened INVALID_REQUEST_TYPE</speak>'
+                        }
+                    },
+                    sessionAttributes: {}
+                });
+            });
     });
 
-    it('responds to a launch event', function() {
-      return request(server)
-        .post('/utahavy')
-        .send({
-          request: {
-            type: 'LaunchRequest',
-          }
-        })
-        .expect(200).then(function(response) {
-          var ssml = response.body.response.outputSpeech.ssml;
-          return expect(ssml).to.eql('<speak>The bottom line for today is, We have stopped issuing avalanche advisories for the 2016/2017 season. Thanks for another great winter and we\'ll see you at the fall fundraiser at Black Diamond September 14th.</speak>');
-        });
+    it('responds to a launch event', function () {
+        return request(server)
+            .post('/utahavy')
+            .send({
+                request: {
+                    type: 'LaunchRequest'
+                }
+            })
+            .expect(200).then(function (response) {
+                var ssml = response.body.response.outputSpeech.ssml;
+
+                return expect(ssml).to.eql('<speak>The bottom line for today is, We have stopped issuing avalanche ' +
+                'advisories for the 2016/2017 season. Thanks for another great winter and we\'ll see you at the fall ' +
+                'fundraiser at Black Diamond September 14th.</speak>');
+            });
     });
 });
 
@@ -63,15 +67,18 @@ describe('parser', function () {
     it('should return the bottom line', function () {
         const html = fs.readFileSync('test/data/report.html', 'utf8');
 
-        expect(parser(html)).to.equal('Mostly Low hazard early this morning will quickly rise to a Moderate risk of loose, wet avalanches with warm temperatures and strong sunshine. There also is a Moderate risk of both human-triggered - as well as natural - cornice falls.  Notoriously unpredictable glide avalanches are also possible in localized terrain.');
+        expect(parser(html)).to.equal('Mostly Low hazard early this morning will quickly rise to a Moderate risk of ' +
+        'loose, wet avalanches with warm temperatures and strong sunshine. There also is a Moderate risk of both ' +
+        'human-triggered - as well as natural - cornice falls.  Notoriously unpredictable glide avalanches are also ' +
+        'possible in localized terrain.');
     });
 });
 
 describe('sanity', function () {
     it('forecast', function () {
-        expect(forecast).not.to.be.null;
+        expect(forecast).not.to.be.null; // eslint-disable-line no-unused-expressions
     });
     it('utahavy', function () {
-        expect(utahavy).not.to.be.null;
+        expect(utahavy).not.to.be.null; // eslint-disable-line no-unused-expressions
     });
 });
